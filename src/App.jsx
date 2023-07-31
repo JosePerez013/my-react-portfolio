ef542cc8531734b88a5d54d5269035adde222d11
JosePerez013 <joseperez951@icloud.com>
2023-07-31 01:16:49 -0700

    finishing navbar stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
 
    return <div>
-    <Navbar />
-
+    <Navbar /> 
    </div>;
 }
 
