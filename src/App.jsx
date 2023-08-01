50569dd140dc50f7ad72518de968e15854a68e29
JosePerez013 <joseperez951@icloud.com>
2023-08-01 14:49:42 -0700

    l

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
 import Socials from "./components/contacts/Socials";
 import About from "./components/about/About";
-import Projects from "./components/projects/Projects";
+import Projects from "./components/work/Projects";
 import Proficiencies from "./components/proficiencies/Proficiencies";
 import Contact from "./components/contact/Contact";
 
